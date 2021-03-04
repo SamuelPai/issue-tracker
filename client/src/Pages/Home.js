40420414a25224061391af235851f4a8bddc0f3f
Samuel Pai <samuelpai16@gmail.com>
2021-03-03 21:02:18 -0800

    able to add pagination display onto the page. Right now functionality doesn't work, so if you click next, it won't work.

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,37 +1,51 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Modal from "../Components/Modal";
 import axios from "axios";
-// import ReactPaginate from "react-paginate";
+import ReactPaginate from "react-paginate";
 
-export default class Home extends Component {
+export default class Home extends PureComponent {
     
     state = {
         offset: 0,
-        issues:[],
+        tableData:[],
         originalData: [],
         perPage: 10,
         currentPage: 0
         
     }
+
+ 
+
     componentDidMount = async () => {
         //make API call here
         let response = await axios.get("http://localhost:3000/api/getIssues");
         let allIssues = response.data;
-        this.setState({issues: allIssues});
+
+        // this determines how many entries will be displayed on the page
+        let slice = allIssues.slice(this.state.offset, this.state.offset + this.state.perPage);
+
+        this.setState({
+            pageCount: Math.ceil(allIssues.length/this.state.perPage),
+            originalData: allIssues,
+            tableData: slice,
+            
+        });
     }
     render() {
         return (
             <div>
                 <table border="1">
                      <thead>
+                         <th>Id</th>
                          <th>Title</th>
                          <th>Issue Number</th>
                          <th>State</th>
                      </thead>
                      <tbody>
                         {
-                          this.state.issues.map((issue, i) => (
+                          this.state.tableData.map((issue, i) => (
                                 <tr>
+                                    <td>{i + 1}</td>
                                     <td>{issue.title}</td>
                                     <td>{issue.number}</td>
                                     <td>{issue.state}</td>
@@ -42,9 +56,21 @@ export default class Home extends Component {
 
                      </tbody>
                  </table>  
-                {/* {this.state.issues.map((issue, index) => (
-                    <h1>{issue.title}</h1>
-                ))} */}
+                
+                 <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}
+                />
+
             </div>
         );
     }
